a60df3b85ad756083f06455a39f60d808e1a1417
Fan Zhang <felicis_zhang@outlook.com>
2020-04-26 18:57:12 -0700

    added state level data module

--- data_fetching.py
+++ data_fetching.py
@@ -26,11 +26,14 @@ df_d = pd.read_csv('./d.csv', delimiter=',', keep_default_na=False)
 # df_r = pd.read_csv('./r.csv', delimiter=',', keep_default_na=False)
 
 data = []
+list_data = []
+list_data_dict = {}
 
 for index, row in df_c.iterrows():
     row_d = df_d.loc[index, :]
     # row_r = df_r.loc[index, :]
     d_item = {}
+
     # if (row['Province_State'] == ''):
     #     d_item['area'] = row['Country/Region']
     # else:
@@ -40,10 +43,25 @@ for index, row in df_c.iterrows():
     d_item['lng'] = row['Long_']
     d_item['confirmed'] = int(row[df_c.columns[-1]])
     d_item['deaths'] = int(row_d[df_c.columns[-1]])
-    # d_item['recovered'] = int(row_r[df_c.columns[-1]])
-    d_item['deaths'] = df_c.columns[-1]
+
+    county = row['Province_State']
+    if county not in list_data_dict:
+        list_data_dict[county] = {'confirmed': int(
+            row[df_c.columns[-1]]), 'deaths': int(row_d[df_c.columns[-1]])}
+    else:
+        list_data_dict[county]['confirmed'] += int(row[df_c.columns[-1]])
+        list_data_dict[county]['deaths'] += int(row_d[df_c.columns[-1]])
+
+    d_item['date'] = df_c.columns[-1]
     data.append(d_item)
 
+for key, value in list_data_dict.items():
+    temp = {'state': key,
+            'confirmed': value['confirmed'], 'deaths': value['deaths']}
+    list_data.append(temp)
 
 with open('./data.json', 'w') as f:
     json.dump(data, f)
+
+with open('./list_data.json', 'w') as f:
+    json.dump(list_data, f)
